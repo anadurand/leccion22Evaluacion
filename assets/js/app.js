a3827f051ec0877532726bef9c7f124d64b76860
Ana Durand <ana.dc1004@gmail.com>
2017-04-17 15:44:16 -0500

    funcion de cerrar imagen

---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+window.addEventListener("load", function(){
+
+  var closed = document.getElementsByClassName("close");
+  var reset = document.getElementById("reset");
+  var origen = document.getElementById("origen");
+  var extincion = document.getElementById("extincion");
+  var contentOrigin = document.getElementById("contentOrigen");
+  var contentExtincion = document.getElementById("contentExtincion");
+
+
+  for(i=0; i<closed.length; i++){
+
+    closed[i].addEventListener("click", function(e){
+      e.target.parentNode.style.display = "none";
+    });
+  }
+  reset.addEventListener("click", function(){
+    for(i=0; i<closed.length; i++){
+      closed[i].parentNode.style.display = "inline-block";
+    }
+  });
+
+  origen.addEventListener("click", function(){
+    contentOrigin.toggleClass("ocultar");
+  });
+
+
+});
